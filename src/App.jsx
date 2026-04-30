6d78352ec15864a7754875d6c67f01837cbb94df
Mudit Gola <muditgola11545@gmail.com>
2026-04-30 19:35:45 +0530

    feat(todo): enhance UI, add icons, and complete CRUD with localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState , useEffect} from "react";
+import { FaBeer } from 'react-icons/fa';
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
@@ -74,29 +77,34 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl bg-blue-200 p-5 min-h-[75vh]"> 
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl bg-blue-200 p-5 min-h-[75vh] md:w-1/2"> 
+      <h1 className="font-bold text-xl text-center">TuDummmm - Manage your daily todos here!!</h1>
         <div className="Add ToDo my-5">
           <h2 className="text-lg font-bold mb-3">Add ToDo</h2>
+          <div className="flex">
+
           <input
             onChange={handleChange}
             value={todo}
             type="text"
-            className="w-3/4 bg-amber-50"
-          />
+            className="px-5 bg-amber-50 rounded-full py-2 w-full"
+            />
           <button
             onClick={handleAdd} disabled={todo.length === 0}
-            className="bg-blue-700 text-white hover:bg-blue-900 rounded-lg p-2 py-1 text-sm font-bold mx-6"
-          >
+            className="bg-blue-700 text-white hover:bg-blue-900 rounded-full p-2 py-2 text-sm font-bold mx-2"
+            >
             SAVE
           </button>
+          </div>
         </div>
         <input onChange={toggleFinished} type="checkbox" checked={showFinished} /> Show Finished
+        <div className="h-[1px] bg-blue-300 my-5 w-[90%] mx-auto"></div>
         <h2 className="text-lg font-bold mb-3">Your ToDo</h2>
         <div className="todos">
           {todos.length === 0 && <div className=" text-shadow-white m-5">No ToDos to display</div>}
           {todos.map((item) => {
             return (
-              (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-between w-1/3 my-2 ">
+              (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-between md:w-full my-2 ">
                 <div className="flex gap-2 flex-1 overflow-hidden">
                 <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} />
                 <div className={`${item.isCompleted ? "line-through" : ""} break-words overflow-hidden`}>
@@ -108,13 +116,13 @@ function App() {
                     onClick={(e) => handleEdit(e,item.id)}
                     className="bg-blue-700 text-white hover:bg-blue-900 rounded-lg p-2 py-1 text-sm font-bold mx-1"
                   >
-                    Edit
+                    <FaEdit />
                   </button>
                   <button
                     onClick={(e) => handleDelete(e,item.id)}
                     className="bg-blue-700 text-white hover:bg-blue-900 rounded-lg p-2 py-1 text-sm font-bold mx-1"
                   >
-                    Delete
+                    <MdDelete />
                   </button>
                 </div>
               </div>
